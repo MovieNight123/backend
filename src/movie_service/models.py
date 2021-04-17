93f2e0d653f1ff83ec4f39016c5eab9eb367e956
Adilet Absatov <adl.absatov@gmail.com>
2021-04-17 15:31:19 +0600

    Implement parsing

--- src/movie_service/models.py
+++ src/movie_service/models.py
@@ -1,45 +1,81 @@
-from django.db import models
 from django.contrib.auth.models import User as BaseUser
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 
 class User(BaseUser):
-    name = models.CharField(max_length=128)
-    photo = models.ImageField(null=True)
+	name = models.CharField(max_length=128)
+	photo = models.ImageField(null=True)
+
+
+class Genre(models.Model):
+	name = models.CharField(max_length=15)
+
+	class Meta:
+		verbose_name = "Genre"
+		verbose_name_plural = "Genres"
+
+	def __str__(self):
+		return self.name
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=128)
-    picture = models.ImageField(null=True)
-    description = models.CharField(max_length=1024)
-    average_rating = models.FloatField()
+	imdb_id = models.CharField(max_length=16, unique=True)
+	movie_id = models.PositiveIntegerField(unique=True, db_index=True)
+	title = models.CharField(max_length=128)
+	picture = models.ImageField(null=True)
+	poster_path = models.CharField(max_length=256)
+	overview = models.CharField(max_length=1024)
+	vote_average = models.FloatField()
+	vote_count = models.IntegerField()
+	popularity = models.FloatField()
+	language = models.CharField(max_length=2)
+	runtime = models.SmallIntegerField()
+	year = models.SmallIntegerField()
+	director_id = models.CharField(max_length=16, null=True)
+	director_name = models.CharField(max_length=128, null=True)
+	budget = models.IntegerField()
+	revenue = models.IntegerField()
+	collection_id = models.IntegerField(null=True)
+
+	class Meta:
+		verbose_name = "Movie"
+		verbose_name_plural = "Movies"
+
+	def __str__(self):
+		return self.title
+
+
+class MovieGenre(models.Model):
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
 
 class Rating(models.Model):
-    value = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	value = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
 
 class WatchListItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
 
 class Review(models.Model):
-    TYPES = (
-        ('POSITIVE', 'POSITIVE'),
-        ('NEGATIVE', 'NEGATIVE'),
-    )
+	TYPES = (
+		('POSITIVE', 'POSITIVE'),
+		('NEGATIVE', 'NEGATIVE'),
+	)
 
-    LANGUAGES = (
-        ('EN', 'EN'),
-        ('RU', 'RU'),
-    )
+	LANGUAGES = (
+		('EN', 'EN'),
+		('RU', 'RU'),
+	)
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    text = models.CharField(max_length=1024)
-    type = models.CharField(choices=TYPES, max_length=20)
-    language = models.CharField(choices=LANGUAGES, max_length=2)
-    created_at = models.DateTimeField(auto_now=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	text = models.CharField(max_length=1024)
+	type = models.CharField(choices=TYPES, max_length=20)
+	language = models.CharField(choices=LANGUAGES, max_length=2)
+	created_at = models.DateTimeField(auto_now=True)
--- src/movie_service/views/auth_views.py
+++ src/movie_service/views/auth_views.py
@@ -1,33 +1,32 @@
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
 from django.db import IntegrityError
-from rest_framework.decorators import action, api_view
-from rest_framework.response import Response
-from rest_framework import mixins, viewsets, serializers, status
+from rest_framework import viewsets, status
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-from rest_framework.views import APIView
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class AuthorizationViewSet(viewsets.ViewSet):
-    def post(self, request):
-        try:
-            user = User.objects.create_user(username=request.data.get('username'),
-                                            email=request.data.get('email'),
-                                            password=request.data.get('password'))
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key, 'username': user.username}, status=status.HTTP_201_CREATED)
-        except IntegrityError:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+	def post(self, request):
+		try:
+			user = User.objects.create_user(username=request.data.get('username'),
+											email=request.data.get('email'),
+											password=request.data.get('password'))
+			token, created = Token.objects.get_or_create(user=user)
+			return Response({'token': token.key, 'username': user.username}, status=status.HTTP_201_CREATED)
+		except IntegrityError:
+			return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
 
-    @action(detail=False, methods=['POST'])
-    def login(self, request):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data.get('user')
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'username': user.username})
+	@action(detail=False, methods=['POST'])
+	def login(self, request):
+		serializer = AuthTokenSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		user = serializer.validated_data.get('user')
+		token, created = Token.objects.get_or_create(user=user)
+		return Response({'token': token.key, 'username': user.username})
 
-    @action(detail=False, methods='DELETE')
-    def logout(request):
-        request.auth.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+	@action(detail=False, methods='DELETE')
+	def logout(request):
+		request.auth.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
