65619de18a6ef2f515124764f702976021cedda9
AldiyarBolat <bolat.aldiar1@gmail.com>
2021-04-19 18:43:50 +0300

    finished movie viewset

--- src/movie_service/views/movie_views.py
+++ src/movie_service/views/movie_views.py
@@ -1,15 +1,15 @@
-import logging
+from django.db.models import QuerySet
 from rest_framework import generics, mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
-from ..models import Movie
+from ..models import Movie, Genre, MovieGenre
 from ..serializers import MovieSerializer
 
 
-class MovieViewSet(mixins.ListModelMixin,
+class MovieListViewSet(mixins.ListModelMixin,
                    mixins.CreateModelMixin,
                    mixins.RetrieveModelMixin,
                    mixins.UpdateModelMixin,
@@ -23,10 +23,36 @@ class MovieViewSet(mixins.ListModelMixin,
         return Movie.objects.all()
 
     def list(self, request, *args, **kwargs):
-        filter_year = request.GET.get('year', 1999)
+        filter_start_year = request.GET.get('start_year', 1999)
+        filter_end_year = request.GET.get('end_year', 1999)
         filter_title = request.GET.get('title', '')
-        sort_by = request.GET.get('sort_by', 'year')  # todo add sort by popularity
-        # todo filter by genre
-        movies = Movie.objects.filter(year=filter_year, title__contains=filter_title).order_by(f'-{sort_by}')
+        sort_by = request.GET.get('sort_by', 'year')
+        filter_genre = request.GET.get('genre')
+
+        movies = Movie.objects.filter(year__gte=filter_start_year, year__lte=filter_end_year,
+                                      title__contains=filter_title).order_by(f'-{sort_by}')
+
+        if filter_genre:
+            genre = Genre.objects.get(name=filter_genre)
+
+            if genre:
+                movie_ids = []
+
+                for movie in movies:
+                    movie_genre = MovieGenre.objects.filter(movie=movie, genre=genre)
+                    if movie_genre:
+                        movie_ids.append(movie.movie_id)
+
+            movies = Movie.objects.filter(movie_id__in=movie_ids)
+
         return Response(MovieSerializer(movies, many=True).data)
 
+
+class MovieApiView(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        movie = Movie.objects.get(movie_id=kwargs['pk'])
+        return Response(MovieSerializer(movie).data)
